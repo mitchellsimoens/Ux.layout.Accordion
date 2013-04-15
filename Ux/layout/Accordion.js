57d046a4706732649630c6fc6623fbce44676b49
William Cheung <william.cheung@ventyx.abb.com>
2013-04-14 21:31:32 -0700

    - Fixed issues #11 & #12 - Fixed code warnings from JSLint and JSHint

--- Ux/layout/Accordion.js
+++ Ux/layout/Accordion.js
@@ -17,91 +17,126 @@ Ext.define('Ux.layout.Accordion', {
         toggleOnTitlebar : false
     },
 
-    constructor: function(container) {
+    setContainer: function (container) {
         this.callParent(arguments);
 
         if (this.getMode() === 'SINGLE') {
             container.on('show', 'checkMode', this, { single : true });
         }
+
+        // Fixed a problem when the first item has 0 height initially causing a "bounce"
+        container.on('show', 'fixItemHeight', this, { single : true});
     },
 
-    checkMode: function(container) {
-        var items = container.getInnerItems(),
-            i     = 0,
-            iNum  = items.length,
-            item, lastItem;
+    // @private
+    fixItemHeight: function (container) {
+        var items = container.getInnerItems();
 
-        for (; i < iNum; i++) {
-            item = items[i];
+        if (items.length > 0) {
+            items[0].setHeight(items[0].element.getHeight());
+        }
+    },
 
+    // @private
+    checkMode: function (container) {
+        var me = this,
+            items = container.getInnerItems(),
+            lastItem = null;
+
+        items.forEach(function (item) {
             if (!item.collapsed) {
                 if (lastItem) {
-                    this.collapse(lastItem);
+                    me.collapse(lastItem);
                 }
 
                 lastItem = item;
             }
+        });
+
+        // Make sure at least one item is expanded
+        if (items.length > 0 && !lastItem) {
+            me.expand(items[0]);
         }
     },
 
-    insertInnerItem: function(item, index) {
-        var me = this;
+    insertInnerItem: function (item, index) {
+        var me = this,
+            titleDock,
+            arrowBtn;
 
         me.callParent([item, index]);
 
-        if (item.isInnerItem()) {
-            var titleDock = item.titleDock = item.insert(0, {
-                    xtype  : 'titlebar',
-                    docked : 'top',
-                    title  : me.container.items.items[index].config.title,
-                    items  : [
-                        {
-                            cls     : me.itemArrowCls,
-                            ui      : 'plain',
-                            align   : 'right',
-                            scope   : me,
-                            handler : 'handleToggleButton'
-                        }
-                    ],
-                    listeners: {
-                      tap: {
-                        fn: function(event, el) {
-                          if (me.getToggleOnTitlebar()) {
+        if (!item.isInnerItem()) {
+            return;
+        }
+
+        titleDock = item.titleDock = item.insert(0, {
+            xtype  : 'titlebar',
+            docked : 'top',
+            title  : me.container.items.items[index].config.title,
+            items  : [
+                {
+                    cls     : me.itemArrowCls,
+                    ui      : 'plain',
+                    align   : 'right',
+                    scope   : me,
+                    handler : 'handleToggleButton'
+                }
+            ],
+            listeners: {
+                tap: {
+                    fn: function () {
+                        if (me.getToggleOnTitlebar()) {
                             me.toggleCollapse(titleDock.up('component'));
-                          }
-                        },
-                        element: 'element'
-                      }
-                    }
-                }),
-                arrowBtn  = item.arrowButton = titleDock.down('button[cls=' + me.itemArrowCls + ']');
+                        }
+                    },
+                    element: 'element'
+                }
+            }
+        });
 
-            item.addCls(me.itemCls);
-            arrowBtn.addCls(me.itemArrowExpandedCls);
+        arrowBtn  = item.arrowButton = titleDock.down('button[cls=' + me.itemArrowCls + ']');
 
-            item.on('painted', function() {
-                item.addCls(me.itemAnimCls);
-            }, me, { single : true });
+        item.addCls(me.itemCls);
+        arrowBtn.addCls(me.itemArrowExpandedCls);
 
-            if (item.collapsed) {
-                item.on('painted', 'collapse', me, { single : true });
-            } else if (me.getMode() === 'SINGLE') {
+        item.on('painted', function () {
+            item.addCls(me.itemAnimCls);
+        }, me, { single : true });
+
+        // Set the collapsed attribute from config, false by default if not set
+        item.collapsed = me.container.items.items[index].config.collapsed;
+
+        if (item.collapsed === undefined) {
+            item.collapsed = false;
+        }
+
+        if (item.collapsed) {
+            item.on('painted', 'collapse', me, { single : true });
+        } else if (me.getMode() === 'SINGLE') {
+            if (!me.getExpandedItem()) {
                 me.setExpandedItem(item);
+            } else {
+                // Collapse the rest of panes
+                item.on('painted', 'collapse', me, { single : true });
             }
         }
     },
 
-    handleToggleButton: function(btn) {
+    handleToggleButton: function (btn) {
         var component = btn.up('titlebar').up('component');
 
         this.toggleCollapse(component);
     },
 
-    toggleCollapse: function(component) {
+    toggleCollapse: function (component) {
         this[component.collapsed ? 'expand' : 'collapse'](component);
     },
 
-    collapse: function(component) {
+    collapse: function (component) {
+        if (!component.isComponent) {
+            component = Ext.getCmp(component.getId());
+        }
         if (component.isInnerItem() && !(this.getMode() === 'SINGLE' && this.getExpandedItem() === component)) {
             var titleDock   = component.titleDock,
                 titleHeight = titleDock.element.getHeight();
@@ -116,13 +151,15 @@ Ext.define('Ux.layout.Accordion', {
         }
     },
 
-    expand: function(component) {
+    expand: function (component) {
         if (component.isInnerItem()) {
             if (this.getMode() === 'SINGLE') {
                 var expanded = this.getExpandedItem();
 
                 this.setExpandedItem(component);
-                this.collapse(expanded);
+                if (expanded) {
+                    this.collapse(expanded);
+                }
             }
 
             component.setHeight(component.fullHeight);
